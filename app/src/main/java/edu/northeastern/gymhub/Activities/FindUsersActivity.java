cb55fb7f75221aa41a61748aa7b7235b2b5da85e
maria <marianpadron@outlook.com>
2023-11-26 23:08:59 -0500

    Got users to display on recycler view

--- app/src/main/java/edu/northeastern/gymhub/Activities/FindUsersActivity.java
+++ app/src/main/java/edu/northeastern/gymhub/Activities/FindUsersActivity.java
@@ -6,7 +6,11 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -25,8 +29,13 @@ public class FindUsersActivity extends AppCompatActivity {
 
     private ArrayList<GymUser> usersList;
     private RecyclerView recyclerView;
+    private FindUsersAdapter.RecyclerClickListener listener;
     private FirebaseDatabase database;
     private DatabaseReference usersRef;
+    private DatabaseReference connectionsRef;
+    private String curUsername;
+    private GymUser curUser;
+    private ImageButton imageButtonBackArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,51 +45,197 @@ public class FindUsersActivity extends AppCompatActivity {
         usersList = new ArrayList<>();
         recyclerView = findViewById(R.id.recyclerViewFindUsers);
 
+        // Acquire user's username
+        SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        curUsername = preferences.getString("Username", "Default User");
+
         // Connect to the database
         database = FirebaseDatabase.getInstance();
         usersRef = database.getReference("users");
+        connectionsRef = database.getReference("connections");
 
-        setAdapter();
+        usersList = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerViewFindUsers);
+
+        // Set buttons
+        imageButtonBackArrow = findViewById(R.id.imageButtonBackArrow);
+        imageButtonBackArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FindUsersActivity.this, ForumActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Go to forum page
+        ImageButton forum = findViewById(R.id.imageButtonForum);
+        forum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FindUsersActivity.this, ForumActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Go to workout page
+        ImageButton workout = findViewById(R.id.imageButtonWorkout);
+        workout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the Workout activity
+                Intent intent = new Intent(FindUsersActivity.this, WorkoutPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
         setUserInfo();
-
+        setAdapter();
     }
 
     private void setAdapter() {
-        FindUsersAdapter adapter = new FindUsersAdapter(usersList);
+        setOnClickListener();
+        FindUsersAdapter adapter = new FindUsersAdapter(usersList, listener);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(adapter);
     }
 
+    private void setOnClickListener() {
+        listener = new FindUsersAdapter.RecyclerClickListener() {
+            @Override
+            public void onFollowButtonClick(View v, int position) {
+                // Perform actions based on the clicked user
+                GymUser clickedUser = usersList.get(position);
+                addNewConnection(clickedUser);
+
+            }
+        };
+    }
+
+    /** Adds the new connection to the users list in the firebase **/
+    private void addNewConnection(GymUser connection) {
+        if (curUsername != null && connection != null) {
+            // Retrieve user data from Firebase
+            DatabaseReference currentUserRef = usersRef.child(curUsername);
+
+            currentUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        // Add user to connections list
+                        GymUser curUser = dataSnapshot.getValue(GymUser.class);
+                        curUser.addConnection(connection.getUsername());
+
+                        // Update database
+                        usersRef.child(curUsername).setValue(curUser)
+                                .addOnCompleteListener(task -> {
+                                    if (task.isSuccessful()) {
+                                        showToast("You are now following " + connection.getName());
+                                    } else {
+                                        showToast("Unable to follow user.");
+                                    }
+                                });
+                    } else {
+                        // Handle the case where the username does not exist in the database
+                        // You can show an error message or take appropriate action
+                        showToast("User not found in the database");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Handle database error
+                    handleDatabaseError(databaseError);
+                }
+            });
+        } else {
+            // Handle null cases
+            showToast("Invalid user or connection");
+        }
+    }
+
+    /** Set users to recycler view **/
+    private void setUserInfo() {
+    // Add a ValueEventListener to retrieve data from the "users" node
+        usersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Clear the existing usersList to avoid duplicates
+                usersList.clear();
+
+                // Loop through each child node under "users"
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    // Retrieve user data
+                    String name = userSnapshot.child("name").getValue(String.class);
+                    String username = userSnapshot.child("username").getValue(String.class);
+                    String password = userSnapshot.child("password").getValue(String.class);
+                    String email = userSnapshot.child("email").getValue(String.class);
+                    String gym = userSnapshot.child("gym").getValue(String.class);
+
+                    // Create a new GymUser object
+                    GymUser gymUser = new GymUser(name, username, password, email, gym);
+
+                    // Add the GymUser to the usersList
+                    usersList.add(gymUser);
+                }
+
+                // Notify the adapter that the data set has changed
+                recyclerView.getAdapter().notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle database error
+                handleDatabaseError(databaseError);
+            }
+        });
+    }
 
 
-//    private void setUserInfo() {
-//        // Add a ValueEventListener to retrieve data from the "users" node
-//        usersRef.addValueEventListener(new ValueEventListener() {
+    /** Fetch current user from database as a GymUser object **/
+    private void getCurrentUser() {
+        if(curUsername != null){
+            // Retrieve user data from Firebase
+            DatabaseReference currentUserRef = usersRef.child(curUsername);
+            currentUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        // Add user to connections list
+                        curUser = dataSnapshot.getValue(GymUser.class);
+                    } else {
+                        // Handle the case where the username does not exist in the database
+                        // You can show an error message or take appropriate action
+                        showToast("User not found in the database");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Handle database error
+                    handleDatabaseError(databaseError);
+                }
+            });
+        } else {
+            // Handle null cases
+            showToast("Invalid user or connection");
+        }
+    }
+
+//        DatabaseReference currentUserRef = usersRef.child(curUsername);
+//
+//        currentUserRef.addListenerForSingleValueEvent(new ValueEventListener() {
 //            @Override
 //            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                // Clear the existing usersList to avoid duplicates
-//                usersList.clear();
+//                if (dataSnapshot.exists()) {
+//                    // Retrieve GymUser data and set curUser
+//                    curUser = dataSnapshot.getValue(GymUser.class);
 //
-//                // Loop through each child node under "users"
-//                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-//                    // Retrieve user data
-//                    String name = userSnapshot.child("name").getValue(String.class);
-//                    String username = userSnapshot.child("username").getValue(String.class);
-//                    String password = userSnapshot.child("password").getValue(String.class);
-//                    String email = userSnapshot.child("email").getValue(String.class);
-//                    String gym = userSnapshot.child("gym").getValue(String.class);
-//
-//                    // Create a new GymUser object
-//                    GymUser gymUser = new GymUser(name, username, password, email, gym);
-//
-//                    // Add the GymUser to the usersList
-//                    usersList.add(gymUser);
+//                } else {
+//                    // Handle the case where the username does not exist in the database
+//                    showToast("User not found in the database");
 //                }
-//
-//                // Notify the adapter that the data set has changed
-//                recyclerView.getAdapter().notifyDataSetChanged();
 //            }
 //
 //            @Override
@@ -89,34 +244,18 @@ public class FindUsersActivity extends AppCompatActivity {
 //                handleDatabaseError(databaseError);
 //            }
 //        });
-//    }
+//
+//        if(curUser== null){
+//            showToast("no current users");
+//        }
+    //}
 
-        private void setUserInfo() {
-        usersRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                usersList.clear();
-
-                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                    // Use getValue with GymUser.class directly
-                    GymUser gymUser = userSnapshot.getValue(GymUser.class);
-
-                    // Add the GymUser to the usersList
-                    usersList.add(gymUser);
-                }
-
-                recyclerView.getAdapter().notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                handleDatabaseError(databaseError);
-            }
-        });
-    }
 
     private void handleDatabaseError(DatabaseError databaseError) {
         Toast.makeText(FindUsersActivity.this, "Database Error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
     }
 
+    private void showToast(String message) {
+        Toast.makeText(FindUsersActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/edu/northeastern/gymhub/Adapters/FindUsersAdapter.java
+++ app/src/main/java/edu/northeastern/gymhub/Adapters/FindUsersAdapter.java
@@ -3,6 +3,7 @@ package edu.northeastern.gymhub.Adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,18 +17,34 @@ import edu.northeastern.gymhub.R;
 public class FindUsersAdapter extends RecyclerView.Adapter<FindUsersAdapter.MyViewHolder> {
 
     private ArrayList<GymUser> usersList;
+    private RecyclerClickListener listener;
 
-    public FindUsersAdapter(ArrayList<GymUser> usersList){
+    public FindUsersAdapter(ArrayList<GymUser> usersList, RecyclerClickListener listener) {
         this.usersList = usersList;
+        this.listener = listener;
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         private TextView userName;
+        private Button buttonFollow;
+        private Button buttonUnfollow;
 
-        public MyViewHolder(final View view){
+        public MyViewHolder(final View view) {
             super(view);
             userName = view.findViewById(R.id.textViewUserName);
+            buttonFollow = view.findViewById(R.id.buttonFollow);
+//            buttonUnfollow = view.findViewById(R.id.buttonUnfollow);
 
+            // Set OnClickListener for buttonFollow
+            buttonFollow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION && listener != null) {
+                        listener.onFollowButtonClick(v, position);
+                    }
+                }
+            });
         }
     }
 
@@ -43,10 +60,31 @@ public class FindUsersAdapter extends RecyclerView.Adapter<FindUsersAdapter.MyVi
     public void onBindViewHolder(@NonNull FindUsersAdapter.MyViewHolder holder, int position) {
         String name = usersList.get(position).getName();
         holder.userName.setText(name);
+
+        //holder.buttonUnfollow.setVisibility(View.GONE);
+
+//        // Check if the current user is in the connections of the displayed user
+//        if (curUser != null) {
+//            String displayedUser = usersList.get(position).getUsername();
+//            boolean isFollowing = curUser.getConnections().contains(displayedUser);
+//
+//            // Set the visibility of buttons based on the relationship between users
+//            if (isFollowing) {
+//                holder.buttonFollow.setVisibility(View.GONE);
+//                holder.buttonUnfollow.setVisibility(View.VISIBLE);
+//            } else {
+//                holder.buttonFollow.setVisibility(View.VISIBLE);
+//                holder.buttonUnfollow.setVisibility(View.GONE);
+//            }
+//        }
     }
 
     @Override
     public int getItemCount() {
         return usersList.size();
+    }
+
+    public interface RecyclerClickListener {
+        void onFollowButtonClick(View v, int position);
     }
 }
