70fede41a56a5275fd6278d97109c67027045c61
maria <marianpadron@outlook.com>
2023-12-02 16:42:12 -0500

    Added fetching profile pic from database

--- app/src/main/java/edu/northeastern/gymhub/Activities/PersonalInformationDetailsPageActivity.java
+++ app/src/main/java/edu/northeastern/gymhub/Activities/PersonalInformationDetailsPageActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.app.Person;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -17,6 +20,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import edu.northeastern.gymhub.R;
 import kotlin.Unit;
@@ -27,13 +32,19 @@ public class PersonalInformationDetailsPageActivity extends AppCompatActivity {
     private ImageView profilePic;
     private ActivityResultLauncher<Intent> imagePickerLauncher;
     Uri selectedImageUri;
+    private Button updateButton;
+    private String curUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_information_details_page);
 
-
+        // Get current user
+        SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        curUsername = preferences.getString("Username", "Default User");
+        
+        
         // Set profile pic
         imagePickerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
                 result -> {
@@ -59,6 +70,15 @@ public class PersonalInformationDetailsPageActivity extends AppCompatActivity {
                             }
                         });
 
+            }
+        });
+
+        // Update button
+        updateButton = findViewById(R.id.buttonUpdateProfile);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateProfile();
             }
         });
 
@@ -91,10 +111,39 @@ public class PersonalInformationDetailsPageActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        
+        
+        getUserPic(curUsername);
+    }
+
+    private void getUserPic(String curUsername) {
+        StorageReference picRef = FirebaseStorage.getInstance().getReference().child("profile_pics").child(curUsername);
+        picRef.getDownloadUrl().addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                Uri uri = task.getResult();
+                setProfilePic(PersonalInformationDetailsPageActivity.this, uri, profilePic);
+            }
+        });
+    }
+
+    private void updateProfile() {
+        StorageReference picRef = FirebaseStorage.getInstance().getReference().child("profile_pics")
+                .child(curUsername);
+
+        if(selectedImageUri != null){
+            picRef.putFile(selectedImageUri).addOnCompleteListener(task -> {
+                showToast("Successfully updated profile pic.");
+            });
+        }
+
     }
 
     private void setProfilePic(Context context, Uri imageUri, ImageView imageView) {
         Glide.with(context).load(imageUri).apply(RequestOptions.circleCropTransform()).into(imageView);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(PersonalInformationDetailsPageActivity.this, message, Toast.LENGTH_SHORT).show();
     }
 
 
