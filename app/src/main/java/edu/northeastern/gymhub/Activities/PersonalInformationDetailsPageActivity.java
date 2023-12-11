962f890ed3031e263f4f27757429541e7f1fe00e
maria <marianpadron@outlook.com>
2023-12-10 19:28:01 -0500

    edited profile activity

--- app/src/main/java/edu/northeastern/gymhub/Activities/PersonalInformationDetailsPageActivity.java
+++ app/src/main/java/edu/northeastern/gymhub/Activities/PersonalInformationDetailsPageActivity.java
@@ -7,16 +7,22 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
@@ -45,6 +51,14 @@ public class PersonalInformationDetailsPageActivity extends AppCompatActivity {
     private Button updateButton;
     private Button logoutButton;
     private String curUsername;
+    private TextView textViewName;
+    private TextView textViewUsername;
+    private TextView textViewUserEmail;
+    private TextView textViewUserGym;
+    private LinearLayout emailLayout;
+    private FirebaseDatabase database;
+    private DatabaseReference usersRef;
+    private String curPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +68,25 @@ public class PersonalInformationDetailsPageActivity extends AppCompatActivity {
         // Get current user
         SharedPreferences preferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
         curUsername = preferences.getString("Username", "Default User");
+
+        // Connect to the database
+        database = FirebaseDatabase.getInstance();
+        usersRef = database.getReference("users");
+
+        // Get objects
+        textViewName = findViewById(R.id.textViewName);
+        textViewUsername = findViewById(R.id.textViewUsername);
+        textViewUserEmail = findViewById(R.id.textViewUserEmail);
+        textViewUserGym = findViewById(R.id.textViewUserGym);
+
+        // Set edit email
+        emailLayout = findViewById(R.id.linearLayoutEmail);
+        emailLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showEmailDialog();
+            }
+        });
 
         // Set profile pic
         imagePickerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
@@ -121,8 +154,101 @@ public class PersonalInformationDetailsPageActivity extends AppCompatActivity {
             }
         });
 
-
         getUserPic(curUsername);
+
+
+        // Retrieve user data from Firebase
+        usersRef.child(curUsername).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String name = dataSnapshot.child("name").getValue(String.class);
+                    String email = dataSnapshot.child("email").getValue(String.class);
+                    String gym = dataSnapshot.child("gym").getValue(String.class);
+                    curPassword = dataSnapshot.child("password").getValue(String.class);
+
+                    // Update UI with retrieved data
+                    textViewName.setText(name);
+                    textViewUsername.setText(curUsername);
+                    textViewUserEmail.setText(email);
+                    textViewUserGym.setText(gym);
+                } else {
+                    // Handle the case where the username does not exist in the database
+                    // You can show an error message or take appropriate action
+                    AndroidUtil.showToast(PersonalInformationDetailsPageActivity.this, "User not found in the database");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle database error
+                AndroidUtil.handleDatabaseError(databaseError);
+            }
+        });
+    }
+
+    private void showEmailDialog() {
+        // Inflate the dialog layout
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_editemail, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogView);
+
+        // Find views in the inflated layout
+        final EditText editTextNewEmail = dialogView.findViewById(R.id.editTextNewEmail);
+        final EditText editPassword = dialogView.findViewById(R.id.editPassword);
+        Button buttonCancel = dialogView.findViewById(R.id.buttonCancel);
+        Button buttonUpdate = dialogView.findViewById(R.id.buttonUpdate);
+
+        // Create and show the dialog
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+
+        // Set click listeners for Cancel and Update buttons
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the new email and password
+                String newEmail = editTextNewEmail.getText().toString().trim();
+                String newPassword = editPassword.getText().toString().trim();
+
+                // Check if the provided email is valid
+                if (!Patterns.EMAIL_ADDRESS.matcher(newEmail).matches()) {
+                    showToast("Please enter a valid email");
+                    return;
+                }
+
+                // Check if new email and password are not empty
+                if (!newEmail.isEmpty() && !newPassword.isEmpty()) {
+                    // Check if the provided password matches the password retrieved from the database
+                    if (newPassword.equals(curPassword)) {
+                        // Passwords match, update email in the database
+                        updateEmail(newEmail);
+                        dialog.dismiss();
+                    } else {
+                        showToast("Incorrect password. Please enter the correct password.");
+                    }
+                } else {
+                    AndroidUtil.showToast(PersonalInformationDetailsPageActivity.this, "Please fill out all fields.");
+                }
+            }
+        });
+    }
+
+    private void updateEmail(String newEmail) {
+        // Update email in Firebase
+        usersRef.child(curUsername).child("email").setValue(newEmail);
+
+        // Update the local UI
+        textViewUserEmail.setText(newEmail);
+
+        AndroidUtil.showToast(PersonalInformationDetailsPageActivity.this, "Email updated");
     }
 
     private void logoutUser() {
